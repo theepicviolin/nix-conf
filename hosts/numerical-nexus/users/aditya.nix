1fe9876822244594fa8995a33cf8a5d1459097f7
Aditya Ramanathan <dev@adityarama.com>
2025-08-04 16:44:11 -0700

    add pfp

--- hosts/numerical-nexus/users/aditya.nix
+++ hosts/numerical-nexus/users/aditya.nix
@@ -130,6 +130,7 @@ with flake.lib;
       "Templates/New ASCII File".text = "";
       "Pictures/Screenshots".source =
         config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/Proton/Pictures/Screenshots";
+      ".face".source = ./R.png; # gnome profile picture
     };
 
     home.stateVersion = "25.05"; # Don't change this unless you know what you're doing!
